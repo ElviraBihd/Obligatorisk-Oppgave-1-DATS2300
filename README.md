26c5a00d04478ca9c2acb99a23796e0e1b4ae33b
elvirabihdan <bigdan.elvira@gmail.com>
2026-09-10 21:52:18 +0200

    Korreksjon i oppgave 1. Oppgave 2 og 3

--- README.md
+++ README.md
@@ -14,9 +14,10 @@ I oppgave 0 lagde jeg metoden gruppeMedlemmer() som skal returnere en verdi av t
 I oppgave 1 lagde jeg metoden maks(int[] a) som finner den største verdien i en tabell. Jeg sammenligner to nabotall om gangen og bytter plass hvis tallet til venstre er størst. Slik flyttes den største verdien til slutten av tabellen, og returneres. Hvis tabellen er tom, kastes en NoSuchElementException.
 
 ### Oppgave 2
-I oppgave 2 gikk vi frem ved å...
+I oppgave 2 teller vi hvor mange ulike verdier det er i en sortert tabell. Først sjekker vi at tabellen er sortert stigende. Deretter sammenligner vi hvert element med det forrige og øker telleren når verdiene er forskjellige. Hvis tabellen ikke er sortert, kastes en `IllegalStateException`. En tom tabell returnerer 0.
 
 ### Oppgave 3
+I oppgave 3 teller vi hvor mange ulike verdier det er i en usortert tabell. For hvert element sjekker vi om den samme verdien finnes tidligere i tabellen. Hvis verdien ikke er funnet tidligere, øker vi telleren. Vi bruker ingen hjelpetabell og endrer ikke innholdet i den originale tabellen. Til slutt returneres antall forskjellige verdier.
 
 ### Oppgave 4
 
--- src/main/java/no/oslomet/cs/algdat/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1.java
@@ -27,13 +27,66 @@ public class Oblig1 {
 
         return a[a.length - 1];
     }
-    public static int ombyttinger(int[] a) {throw new UnsupportedOperationException();}
+    public static int ombyttinger(int[] a) {
+        int antall = 0;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                antall++;
+            }
+        }
+
+        return antall;
+    }
 
     // Oppgave 2
-    public static int antallUlikeSortert(int[] a) {throw new UnsupportedOperationException();}
+    public static int antallUlikeSortert(int[] a) {
+        if (a.length == 0) {
+            return 0;
+        }
 
+        // tabell sortert stigende sjekk
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i - 1]) {
+                throw new IllegalStateException("Tabellen er ikke sortert stigende!");
+            }
+        }
+
+        int antall = 1;
+
+        // teller forskjellige verdier
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] != a[i - 1]) {
+                antall++;
+            }
+        }
+
+        return antall;
+    }
     // Oppgave 3
-    public static int antallUlikeUsortert(int[] a) {throw new UnsupportedOperationException();}
+    public static int antallUlikeUsortert(int[] a) {
+        int antall = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            boolean finnesFraFor = false;
+
+            for (int j = 0; j < i; j++) {
+                if (a[i] == a[j]) {
+                    finnesFraFor = true;
+                    break;
+                }
+            }
+
+            if (!finnesFraFor) {
+                antall++;
+            }
+        }
+
+        return antall;
+    }
 
     // Oppgave 4
     public static void sorter(int[] a, int fra, int til) {throw new UnsupportedOperationException();}
